06cb6755e84a66c06398ae78a1b4e3838c767830
tejaswini <tejaswini@abc.com>
2020-09-24 22:33:04 +0530

    testing

--- MyProject1/src/test/java/MyTest.java
+++ MyProject1/src/test/java/MyTest.java
@@ -1,6 +1,6 @@
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class MyTest {
 
